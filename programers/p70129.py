1c4662bc1ae16935a4b0f50d3b1b634da8adeaf1
지민 <wlals425315@gmail.com>
2022-11-03 06:16:00 +0900

    refactor

--- programers/p70129.py
+++ programers/p70129.py
@@ -1,9 +1,8 @@
 # 이진 반복하기 
 def solution(s):
-    changes=0
-    zero_count=0
+    changes,zero_count=0, 0
     s=list(s)
-    while not(s.count('1')== 1 and len(s)==1):
+    while not(len(s)==1):
         zero_count+=s.count('0')
         s=list(str(format(s.count('1'),'b')))
         changes+=1
