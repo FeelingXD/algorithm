13df22bf63989b7cc675c8a7699975d923b36819
지민 <wlals425315@gmail.com>
2022-11-11 03:44:33 +0900

    check ignore problem

--- programers/p59043.sql
+++ programers/p59043.sql
@@ -1,7 +1,6 @@
-/*O= out, I= In*/
+/*O= out, I= In check ignore*/
 select O.animal_id,O.name
 from animal_ins I join animal_outs O
 on I.animal_id =O.animal_id
 where I.datetime> O.datetime 
 ORDER BY I.DATETIME;
-
