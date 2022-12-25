aece2787db7a95cc7a4ded72950dc0c62a7c5c13
jimin <wlals425315@gmail.com>
2022-12-26 01:24:40 +0900

    Update: 계수정렬 풀이 추가

--- CodingTest/Binarysearch/find_gear.py
+++ CodingTest/Binarysearch/find_gear.py
@@ -21,8 +21,22 @@ arr_n = list(map(int, sys.stdin.readline().split()))
 m = int(sys.stdin.readline())
 arr_m = list(map(int, sys.stdin.readline().split()))
 
+# for loop
 for value in arr_m:
     if binary_search(arr_n, value, 0, len(arr_m)) != None:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
+
+# 계수정렬
+
+count_arr = [0] * (max(arr_n) + 1)
+
+for i in arr_n:
+    count_arr[i] = 1
+print('')
+for i in arr_m:
+    if count_arr[i] == 1:
         print('yes', end=' ')
     else:
         print('no', end=' ')
