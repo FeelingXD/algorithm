aa83ae4656d4b14b5234694d3397b952ac97e236
jimin <wlals425315@gmail.com>
2022-12-27 04:04:05 +0900

    Add: 디익스트라알고리즘

---
+++ CodingTest/ShotestPath/Dijkstra.py
@@ -0,0 +1,51 @@
+# 디익스트라 알고리즘
+import sys
+input = sys.stdin.readline
+INF = int(1e9)  # 무한
+
+n, m = map(int, input().split())
+
+start = int(input())
+
+# init
+graph = [[] for i in range(n+1)]
+visited = [False] * (n+1)
+distance = [INF] * (n+1)
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    # a:node to b:node => cost: c
+    graph[a].append((b, c))
+
+
+def get_smallest_node():
+    min_value = INF
+    index = 0
+    for i in range(1, n+1):
+        if distance[i] < min_value and not visited[i]:
+            min_value = distance[i]
+            index = i
+    return index
+
+
+def dijkstra(start: int):
+    distance[start] = 0
+    visited[start] = True
+    for j in graph[start]:
+        distance[j[0]] = j[1]
+    for i in range(n-1):
+        now = get_smallest_node()
+        visited[now] = True
+        for j in graph[now]:
+            cost = distance[now]+j[1]
+            if cost < distance[j[0]]:
+                distance[j[0]] = cost
+
+
+dijkstra(start)
+
+for i in range(1, n+1):
+    if distance[i] == INF:
+        print("INF")
+    else:
+        print(distance[i])
