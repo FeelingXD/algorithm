9e07ae2dd575cea834f0698e1eeade7b6cc70e5f
jimin <wlals425315@gmail.com>
2023-01-01 03:04:46 +0900

    Update: 크루스칼알고리즘

--- CodingTest/Graph/kruskal.py
+++ CodingTest/Graph/kruskal.py
@@ -1,6 +1,51 @@
 # 크루스칼알고리즘
 # 최소신장트리
+
+# example input
+# 7 9
+# 1 2 29
+# 1 5 75
+# 2 3 35
+# 2 6 34
+# 3 4 7
+# 4 6 23
+# 4 7 13
+# 5 6 53
+# 6 7 25
+import sys
+input = sys.stdin.readline
+
+
 def find_parent(parent, x):
     if parent[x] != x:
         return find_parent(parent, parent[x])
     return parent[x]
+
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+v, e = map(int, input().split())
+parent = [i for i in range(v+1)]  # parent 를 노드 수 리스트생성
+edges = []
+result = 0
+
+for _ in range(e):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+
+edges.sort(key=lambda x: x[0])
+
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+
+print(result)
