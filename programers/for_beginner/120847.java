8a244cc668fd992bac2aba362af30cc511147eac
jimin <wlals425315@gmail.com>
2023-01-12 17:25:55 +0900

    solve: 최댓값 만들기

---
+++ programers/for_beginner/120847.java
@@ -0,0 +1,11 @@
+//최댓값 만들기
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        Arrays.sort(numbers);
+        
+        return numbers[numbers.length-1]*numbers[numbers.length-2];
+    }
+}
