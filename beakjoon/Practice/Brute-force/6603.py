732d0b33fb91a0473adcdd42461043dbdcdc3a1c
jimin <wlals425315@gmail.com>
2023-01-25 21:58:35 +0900

    solve: 로또

---
+++ beakjoon/Practice/Brute-force/6603.py
@@ -0,0 +1,10 @@
+# 로또
+from itertools import combinations
+import sys
+input = sys.stdin.readline
+while True:
+    tmp = list(map(int, input().split()))
+    if tmp[0] == 0:
+        break
+    for i in combinations(tmp[1:], 6):
+        print(' '.join([str(x) for x in i]))
