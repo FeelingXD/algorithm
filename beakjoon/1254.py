8abc0819fb41af56404111e3a60f5ec11943e45a
jimin <wlals425315@gmail.com>
2023-01-31 08:47:13 +0900

    solve: 펠린드롬만들기

---
+++ beakjoon/1254.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+text = input().strip()
+
+
+def len_pelindrom(text):
+    for i in range(len(text)):
+        if text[i:] == text[i:][::-1]:
+            return len(text[i:])
+
+
+pellen = len_pelindrom(text)
+print(len_pelindrom(text) if pellen == len(
+    text) else len(text)+(len(text)-pellen))
