1f4705b1a68de9e02aae97a667900ec21b2f5ccd
FeelingXD <wlals425315@gmail.com>
2023-02-11 18:03:45 +0900

    solve: 반복수열

---
+++ beakjoon/2331.py
@@ -0,0 +1,23 @@
+# 반복수열
+import sys
+input = sys.stdin.readline
+
+num, num2 = map(int, input().split())
+
+dp = [0]*25000
+dp[1] = num
+cast = []
+index = 1
+while True:
+    tot = 0
+    tmp = dp[index]
+    index += 1
+    while tmp > 0:
+        tot += pow(tmp % 10, num2)
+        tmp //= 10
+
+    if tot in dp:
+        print(dp.index(tot)-1)
+        break
+    else:
+        dp[index] = tot
