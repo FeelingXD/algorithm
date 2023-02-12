87db4877199931c3caf989405ca8d78ea323d17c
FeelingXD <wlals425315@gmail.com>
2023-02-13 00:22:20 +0900

    solve: 2×n 타일링 2

---
+++ beakjoon/11727.py
@@ -0,0 +1,16 @@
+# 2*n 타일링 2
+
+import sys
+input = sys.stdin.readline
+
+
+n = int(input())
+
+dp = [0]*(1001)
+dp[1] = 1
+dp[2] = 3
+dp[3] = 5
+dp[4] = 9
+for i in range(4, n+1):
+    dp[i] = dp[i-1]+2*dp[i-2]
+print(dp[n] % 10007)
