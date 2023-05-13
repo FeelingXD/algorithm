0083da6d5de17f55f560ac809ca1f97aafa7b47a
FeelingXD <wlals425315@gmail.com>
2023-05-13 12:55:39 +0900

    solve: 전화번호 목록

---
+++ programers/42577.py
@@ -0,0 +1,8 @@
+# 전화번호 목록
+def solution(phone_book):
+    phone_book.sort()
+
+    for p1, p2 in zip(phone_book, phone_book[1:]):
+        if p2.startswith(p1):
+            return False
+    return True
