cd3030dc0cb2444012dbb4e610f2e4c76acadd40
FeelingXD <wlals425315@gmail.com>
2023-07-08 08:42:36 +0900

    solve: a 와 b 출력하기

---
+++ programers/basic_training/181951.java
@@ -0,0 +1,13 @@
+// a와 b 출력하기
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(String.format("a = %d",a));
+        System.out.println(String.format("b = %d",b));
+    }
+}
