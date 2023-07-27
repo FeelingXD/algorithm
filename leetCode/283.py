539413f849f5c5a64458eac28c012d3510a02086
FeelingXD <wlals425315@gmail.com>
2023-07-27 10:49:34 +0900

    solve : move zeros

---
+++ leetCode/283.py
@@ -0,0 +1,8 @@
+# move zeros (0 옮기기)
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        nums.sort(key=lambda x: x == 0)
