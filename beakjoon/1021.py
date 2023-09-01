d6155bc5de95876f867b9fd2f83a1b80c39336b7
FeelingXD <wlals425315@gmail.com>
2023-09-01 23:26:51 +0900

    solve: 회전하는 큐

---
+++ beakjoon/1021.py
@@ -0,0 +1,24 @@
+# 회전하는큐
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+data = deque([i for i in range(1, n+1)])
+index = list(map(int, input().split()))
+
+count = 0
+for num in index:
+    while 1:
+        if data[0] == num:
+            data.popleft()
+            break
+        else:
+            if data.index(num) <= len(data)//2:
+                data.rotate(-1)
+                count += 1
+            else:
+                data.rotate(1)
+                count += 1
+
+print(count)
