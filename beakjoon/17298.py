4a8f3df0a275d1037b2a3cee457fbb038bfe69c4
FeelingXD <wlals425315@gmail.com>
2023-10-25 05:03:12 +0900

    solve: 오큰수

---
+++ beakjoon/17298.py
@@ -0,0 +1,15 @@
+# 오큰수
+import sys
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    n = int(input())
+    li = list(map(int, input().split()))
+    answer = [-1]*n
+    stack = []
+    for v in range(1, n):
+        while stack and li[stack[-1]] < li[v]:
+            answer[stack.pop()] = li[v]
+        stack.append(v)
+    print(*answer)
+    pass
