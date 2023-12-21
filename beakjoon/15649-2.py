f2aa8243c915146348cd5f471852844c1d47a696
FeelingXD <wlals425315@gmail.com>
2023-12-21 18:29:30 +0900

    solve: N과 M(1)

---
+++ beakjoon/15649-2.py
@@ -0,0 +1,25 @@
+#N과 M 
+import sys
+input =sys.stdin.readline
+answer=[]
+def back(li):
+    global M,N
+    if len(li)==M:
+        answer.append(li[:])
+        return
+    else:
+        for i in range(1,N+1):
+            if i in li:
+                continue
+            back(li+[i])
+            
+def solution():
+    global N,M,answer
+    N,M=map(int,input().split())
+    back([])
+    for v in answer:
+        print(*v)
+    pass
+if __name__=="__main__": # 실행되는 부분
+    solution()
+    pass
