0f70343e2c9557621bf35b392571aa0885725ebf
FeelingXD <wlals425315@gmail.com>
2023-12-24 20:00:32 +0900

    solve: N과 M (7)

---
+++ beakjoon/15656.py
@@ -0,0 +1,28 @@
+#N과 M (7)
+import sys
+input =sys.stdin.readline
+answer=[]
+numbers=[]
+def back(li,):
+    global M,N,answer,visited
+    if len(li)==N:
+        answer.append(li)
+        return
+    else:
+        for i in range(len(numbers)):
+                visited[i]=True
+                back(li+[numbers[i]])
+                visited[i]=False
+    pass
+def solution():
+    global M,N,numbers,visited
+    M,N=map(int,input().split())
+    numbers=sorted(list(map(int,input().split())))
+    visited=[False]*M
+    back([])
+    for v in answer:
+        print(*v)
+    pass
+if __name__=="__main__": # 실행되는 부분
+    solution()
+    pass
