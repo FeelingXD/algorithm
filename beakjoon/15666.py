8b5b113875b7ab9f8d31ec4c6ccec517d0bda3a3
FeelingXD <wlals425315@gmail.com>
2023-12-26 05:49:19 +0900

    solve: N과 M (12)

---
+++ beakjoon/15666.py
@@ -0,0 +1,28 @@
+#N과 M (12)
+import sys
+input =sys.stdin.readline
+answer=[]
+numbers=[]
+def back(li):
+    global M,N,answer,visited
+    if len(li)==N:
+        answer.append(li)
+        return
+    else:
+        for i in range(len(numbers)):
+            if li and li[-1]>numbers[i]:
+                continue
+            back(li+[numbers[i]])
+    pass
+def solution():
+    global M,N,numbers,visited
+    M,N=map(int,input().split())
+    numbers=sorted(list((set(map(int,input().split())))))
+    visited=[False]*M
+    back([])
+    for v in answer:
+        print(*v)
+    pass
+if __name__=="__main__": # 실행되는 부분
+    solution()
+    pass
