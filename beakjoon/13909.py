c0333842cbb80437681bdb36c990215c229dfb52
FeelingXD <wlals425315@gmail.com>
2024-02-18 11:20:04 +0900

    solve: 창문 닫기

---
+++ beakjoon/13909.py
@@ -0,0 +1,15 @@
+# 창문 닫기
+import sys
+
+input = sys.stdin.readline
+
+
+def solution():
+    n = int(input())
+    print(int(n ** (1 / 2)))
+    pass
+
+
+if __name__ == "__main__":  # 실행되는 부분
+    solution()
+    pass
