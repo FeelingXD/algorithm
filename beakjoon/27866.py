7a11359cedda709964ad72a580f5fd6a8c7a2bcd
FeelingXD <wlals425315@gmail.com>
2024-04-13 19:25:53 +0900

    solve: 문자와 문자열

---
+++ beakjoon/27866.py
@@ -0,0 +1,14 @@
+# 문자와 문자열
+import sys
+
+input = sys.stdin.readline
+
+
+def solution():
+    print(input().strip()[int(input()) - 1])
+    pass
+
+
+if __name__ == "__main__":  # 실행되는 부분
+    solution()
+    pass
